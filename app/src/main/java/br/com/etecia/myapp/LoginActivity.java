40d52e2293508e6b68181b1fd69c6ed1165f8874
maureco-lab <mauriciomrm04@gmail.com>
2025-10-02 21:38:40 -0300

    java

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText txtSenha, txtEmail;
+    MaterialButton btnEntrar, btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,24 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        txtEmail= findViewById(R.id.txtEmail);
+        txtSenha= findViewById(R.id.txtSenha);
+        btnEntrar= findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, senha;
+                email=txtEmail.getText().toString();
+                senha=txtSenha.getText().toString();
+
+                if (email.equals("etecia") && senha.equals("12345")){
+
+                } else{
+                    Toast.makeText(getApplicationContext(),"Usuário ou senha incorreto", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }

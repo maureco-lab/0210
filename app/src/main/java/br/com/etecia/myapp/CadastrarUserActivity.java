a8e58cc825aac0423627975e8dba88b5d60ca42d
maureco-lab <mauriciomrm04@gmail.com>
2025-10-02 22:18:56 -0300

    java

---
+++ app/src/main/java/br/com/etecia/myapp/CadastrarUserActivity.java
@@ -0,0 +1,40 @@
+package br.com.etecia.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+public class CadastrarUserActivity extends AppCompatActivity {
+
+    MaterialButton btnVoltarcadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.cadastrar_user_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnVoltarcadastro= findViewById(R.id.btnVoltarcadastro);
+
+        btnVoltarcadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}

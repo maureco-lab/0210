3168c52dd698f39982ed51c5f73d726496fc7eba
maureco-lab <mauriciomrm04@gmail.com>
2025-10-30 22:07:59 -0300

    menu

--- app/src/main/java/br/com/etecia/myapp/RedefinirSenhaActivity.java
+++ app/src/main/java/br/com/etecia/myapp/RedefinirSenhaActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RedefinirSenhaActivity extends AppCompatActivity {
+
+    TextView txtVoltarRedefinir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,16 @@ public class RedefinirSenhaActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txtVoltarRedefinir= findViewById(R.id.txtVoltarRedefinir);
+
+        txtVoltarRedefinir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RecuperarSenhaActivity.class));
+                finish();
+            }
         });
     }
 }

92887cab4e54d038b014584d667becb646e4ad14
maureco-lab <mauriciomrm04@gmail.com>
2025-11-13 22:00:18 -0300

    cards

---
+++ app/src/main/java/br/com/etecia/myapp/SolFragment.java
@@ -0,0 +1,25 @@
+package br.com.etecia.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SolFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sol, container, false);
+
+
+
+        return view;
+    }
+}

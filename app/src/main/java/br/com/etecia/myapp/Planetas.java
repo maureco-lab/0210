f511a71f6d57cdcfd649992342c73d1fc0e589c8
maureco-lab <mauriciomrm04@gmail.com>
2025-11-27 20:21:26 -0300

    get and setter

---
+++ app/src/main/java/br/com/etecia/myapp/Planetas.java
@@ -0,0 +1,34 @@
+package br.com.etecia.myapp;
+
+public class Planetas {
+    private String titulo, subtitulo;
+    private int imgPlaneta;
+
+    //Construtor vazio
+    public Planetas() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSubtitulo() {
+        return subtitulo;
+    }
+
+    public void setSubtitulo(String subtitulo) {
+        this.subtitulo = subtitulo;
+    }
+
+    public int getImgPlaneta() {
+        return imgPlaneta;
+    }
+
+    public void setImgPlaneta(int imgPlaneta) {
+        this.imgPlaneta = imgPlaneta;
+    }
+}

ffdbfdad2b2577f0adbba7b7f1352df6dd005d6d
maureco-lab <mauriciomrm04@gmail.com>
2025-12-04 21:17:47 -0300

    holder

--- app/src/main/java/br/com/etecia/myapp/ListaPlanetasActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaPlanetasActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class ListaPlanetasActivity extends AppCompatActivity {
+
+    List<Planetas> lstPlanetas;
+    RecyclerView listaplaneta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,7 @@ public class ListaPlanetasActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Recycle view
+        listaplaneta = findViewById(R.id.listaplaneta);
     }
 }
